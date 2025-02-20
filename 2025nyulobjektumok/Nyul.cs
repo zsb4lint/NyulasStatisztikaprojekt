3d3b9d3eed149e85ba4f12dca2f4200e4bcddf2d
Zsidákovits Bálint <zsidakovits.balint@ckik.loc>
2025-02-20 13:58:08 +0100

    a classok letrehozasa

---
+++ 2025nyulobjektumok/Nyul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025nyulobjektumok
+{
+    internal class Nyul
+    {
+        int kor, xkordinata, ykordinata;
+
+        public Nyul(int kor, int xkordinata, int ykordinata)
+        {
+            this.kor = kor;
+            this.xkordinata = xkordinata;
+            this.ykordinata = ykordinata;
+        }
+    }
+}
---
+++ 2025nyulobjektumok/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025nyulobjektumok
+{
+    internal class Program
+    {
+        static List<Nyul> lista = new List<Nyul>();
+        static void Main(string[] args)
+        {
+
+            Fajlbeolvasas();
+            Console.ReadLine();
+        }
+        static void Fajlbeolvasas()
+        {
+            StreamReader f = new StreamReader("nobel.csv");
+            f.ReadLine();
+            while (!f.EndOfStream)
+            {
+                string[] st = f.ReadLine().Split(';');
+                Nyul sv = new Nyul(Convert.ToInt32(st[0]), Convert.ToInt32(st[1]), Convert.ToInt32(st[2]));
+                lista.Add(sv);
+            }
+            f.Close();
+        }
+    }
+}
