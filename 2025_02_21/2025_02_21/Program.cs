057e87668de2981eda081314fa472b5162f0e641
zsb4lint <94898338+zsb4lint@users.noreply.github.com>
2025-02-26 11:26:17 +0100

    Add files via upload

---
+++ 2025_02_21/2025_02_21/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_02_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
